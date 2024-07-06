ed30da8b4636c0ab59254257423cbadee14f3cdf
devadhit <adhitiazainn@gmail.com>
2024-07-06 21:56:44 +0700

    add: toastr notification

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -27,6 +27,7 @@ namespace BulkyWeb.Controllers
 			if (ModelState.IsValid) {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+				TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
 			return View(obj);
@@ -52,6 +53,7 @@ namespace BulkyWeb.Controllers
 			{
 				_db.Categories.Update(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
 				return RedirectToAction("Index");
 			}
 			return View(obj);
@@ -80,6 +82,7 @@ namespace BulkyWeb.Controllers
 			}
 			_db.Categories.Remove(obj);
 			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
 			return RedirectToAction("Index");
 		}
 	}
